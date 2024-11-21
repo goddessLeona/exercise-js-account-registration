7f39bbdb7eda26207d0ecb8ed5b33cc5ebee73d4
unknown <petra.j-son@outlook.com>
2024-11-21 10:01:32 +0100

    started with first input

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const fieldest = document.querySelector(".nameInput");
+const input = document.querySelector(".form .nameInput");
 
+const valid7Letters = (value) => value.length >= 7;
+
+input.addEventListener("input", (event) => {
+  const value = event.target.value;
+
+  if (valid7Letters(value)) {
+    console.log("value is valid");
+    fieldest.classList.add("valid");
+    fieldest.classList.remove("error");
+  } else {
+    console.log("value is NOT valid");
+    fieldest.classList.remove("valid");
+    fieldest.classList.add("error");
+  }
+});
