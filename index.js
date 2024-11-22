ebff72509abd2264deaeba981e5691f858c79823
unknown <petra.j-son@outlook.com>
2024-11-22 11:56:34 +0100

    Finished Name + username not looking mazing jet

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const fieldest = document.querySelector(".nameInput");
 const input = document.querySelector(".form .nameInput");
+const nameLabel = document.querySelector(".nameLabel");
+const validator = document.querySelector(".ckeckOk");
+const userLabel = document.querySelector(".nameUsername");
+const userInput = document.querySelector(".usernameInput");
+const inputPassword = document.querySelector(".paswordInput");
+const inputPasswordC = document.querySelector(".paswordInputConfirm");
+const labelPassword = document.querySelector(".namePassword");
 
-const valid7Letters = (value) => value.length >= 7;
 
-input.addEventListener("input", (event) => {
+// cklick on label get focus on the input 
+
+nameLabel.addEventListener("click", (event) =>{
+    console.log("it is working");
+    fieldest.focus();
+})
+
+userLabel.addEventListener("click", (event) =>{
+    console.log("working")
+    userInput.focus();
+})
+
+// get Password checked min 8 characters
+
+const valid8Letters = (value) => value.length >= 8;
+
+inputPassword.addEventListener("input", (event) => {
   const value = event.target.value;
 
-  if (valid7Letters(value)) {
+  if (valid8Letters(value)) {
     console.log("value is valid");
-    fieldest.classList.add("valid");
-    fieldest.classList.remove("error");
+    validator.classList.add("valid");
+    validator.classList.remove("error");
   } else {
     console.log("value is NOT valid");
-    fieldest.classList.remove("valid");
-    fieldest.classList.add("error");
+    validator.classList.remove("valid");
+    validator.classList.add("error");
   }
 });
+
+// confirm the password
+
+inputPasswordC.addEventListener("input", (event) =>{
+   
+})
